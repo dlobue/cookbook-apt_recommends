fec9c70d33585d501313b9a374cf5432bfccd5b7
Dominic LoBue <dominic.lobue@gmail.com>
2015-04-27 12:05:11 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Dominic LoBue"
+maintainer_email "dominic.lobue@gmail.com"
+license          "Apache 2.0"
+description      "apt cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README'))
+version          "0.0.1"
+
