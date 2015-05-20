6178c79396eb1a5ba2e8b308671b3b5ac1b52957
Dominic LoBue <dominic.lobue@gmail.com>
2015-05-20 14:24:14 -0700

    fix typo

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             "apt_recommend"
+name             "apt_recommends"
 maintainer       "Dominic LoBue"
 maintainer_email "dominic.lobue@gmail.com"
 license          "Apache 2.0"
-description      "apt cookbook"
+description      "apt_recommends cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README'))
 version          "0.0.1"
 
